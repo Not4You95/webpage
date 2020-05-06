c695d6b726db6fd686ebe0fd405aa62fb741e8c6
not4you <jonekm@kth.se>
2020-05-06 16:42:44 +0200

    login fix

--- login.php
+++ login.php
@@ -7,26 +7,82 @@
     <title>NOT4YOU &#8211; F#!& OFF</title>
     <link rel="stylesheet" id='boostramp' href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel='stylesheet' id='fullscreenly-parent-style-css' href='https://not4you.se/text/style.css' type='text/css' media='all' />
+    <link rel='stylesheet' id='fullscreenly-parent-style-css' href='https://not4you.se/text/newStyle.css' type='text/css' media='all' />
     <link rel='stylesheet' id='dashicons-css' href='https://not4you.se/text/dashicons.min.css?ver=5.4.1' type='text/css' media='all' />
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
- 
 
- 
+
+
 
 
 </head>
 
 <body>
 
- 
- <div class="jumbotron text-center bg-dark" style="margin-bottom:0">
 
- <a href="https://not4you.se/" rel="home" class="logo">NOT4YOU</a> 
-</div>
+    <div class="jumbotron text-center bg-dark" style="margin-bottom:0">
+
+        <a href="https://not4you.se/" rel="home" class="logo">NOT4YOU</a>
+    </div>
+
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <div class="login100-pic js-tilt" data-tilt>
+                    <img src="images/img-01.png" alt="IMG">
+                </div>
+
+                <form class="login100-form validate-form">
+                    <span class="login100-form-title">
+                        Member Login
+                    </span>
+
+                    <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+                        <input class="input100" type="text" name="email" placeholder="Email">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="wrap-input100 validate-input" data-validate="Password is required">
+                        <input class="input100" type="password" name="pass" placeholder="Password">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-lock" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn">
+                            Login
+                        </button>
+                    </div>
+
+                    <div class="text-center p-t-12">
+                        <span class="txt1">
+                            Forgot
+                        </span>
+                        <a class="txt2" href="#">
+                            Username / Password?
+                        </a>
+                    </div>
+
+                    <div class="text-center p-t-136">
+                        <a class="txt2" href="#">
+                            Create your Account
+                            <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
 
 
     <div class="container" id="start">
