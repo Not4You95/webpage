0cd8605ac26acf5429c3153f4ece01e4bf255b07
not4you <jonekm@kth.se>
2020-05-06 11:22:45 +0200

    login update

--- login.php
+++ login.php
@@ -3,155 +3,102 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-   
+
     <title>NOT4YOU &#8211; F#!& OFF</title>
-    <link rel="stylesheet" id = 'boostramp' href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-  <link rel='stylesheet' id='fullscreenly-parent-style-css'
-        href='https://not4you.se/text/style.css'
-        type='text/css' media='all' />
+    <link rel="stylesheet" id='boostramp' href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel='stylesheet' id='fullscreenly-parent-style-css' href='https://not4you.se/text/style.css' type='text/css' media='all' />
 </head>
 
 <body>
-    <div id="page" class="site has-sidebar">
-        <div class="header">
-            <div class="image-creativeily-header"></div>
-            <div class="header-top">
-                <!-- logo -->
 
-                <a href="https://not4you.se/" rel="home" class="logo">NOT4YOU</a>
-
-                <!-- Mobile Bar & Menu Icon -->
-
-                <input type="checkbox" class="menu-toggle" id="menu-toggle">
-                <div class="mobile-bar">
-                    <label tabindex="0" class="menu-icon">
-                        <span></span>
-                    </label>
-                </div>
-                <div class="header-menu">
-                    <nav id="nav" class="top-navigation" role="navigation" aria-label="Top Menu">
-                        <span id="creativeily-menu-back"><span class="dashicons dashicons-arrow-left-alt2"></span></span>
-                        <span id="creativeily-menu-home"><span class="dashicons dashicons-admin-home"></span></span>
-
-                        <div id="top-menu" class="menu">
-                            <ul id="menu-menu-1" class="">
-                                <li id="menu-item-18" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-18">
-                                    <a href="http://not4you.se/" aria-current="page">Home</a></li>
-                                <li id="menu-item-19" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-19">
-                                    <a href="https://not4you.se/getstudentinfo.php">Show
-                                        table</a></li>
-                                <li id="menu-item-24" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-24">
-                                    <a href="https://not4you.se/login.php">LOGIN</a>
-                                </li>
-                            </ul>
-                        </div> <a href="#" id="accessibility-close-menu"></a>
-                    </nav><!-- #site-navigation -->
-                </div>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
 
-            </div>
+        <nav id="nav" class="top-navigation" role="navigation" aria-label="Top Menu">
+            <span id="creativeily-menu-back"><span class="dashicons dashicons-arrow-left-alt2"></span></span>
+            <span id="creativeily-menu-home"><span class="dashicons dashicons-admin-home"></span></span>
 
-            <div class="info">
-                <h1>YOUR HOME IN YOUR HANDS</h1>
-
-                <div class="meta">
-                    <p class="site-description">The power of internet</p>
-                </div>
-
-
-            </div>
-
-            <a href="#start" id="section06"><span></span></a>
-        </div><!-- .custom-header -->
+            <div id="top-menu" class="menu">
+                <ul id="menu-menu-1" class="">
+                    <li id="menu-item-18" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-18">
+                        <a href="http://not4you.se/" aria-current="page">Home</a></li>
+                    <li id="menu-item-19" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-19">
+                        <a href="https://not4you.se/getstudentinfo.php">Show
+                            table</a></li>
+                    <li id="menu-item-24" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-24">
+                        <a href="https://not4you.se/login.php">LOGIN</a>
+                    </li>
+                </ul>
+            </div> <a href="#" id="accessibility-close-menu"></a>
+        </nav><!-- #site-navigation -->
 
 
 
 
-        <div class="site-content-contain" id="start">
-            <div id="content" class="site-content">
-
-                <div class="wrapmain">
-
-                    <div id="primary" class="content-area">
-                        <main id="main" class="site-main" role="main">
-
-                            <article id="post-1" class="post-1 post type-post status-publish format-standard hentry category-uncategorised">
-
-                                <header class="entry-header">
-                                    <h2 class="entry-title"><a href="https://not4you.se/?p=1" rel="bookmark">Add new student!</a></h2>
-                                    <div class="postinfo">
-                                    <!-- add text -->
-                                       
-                                    </div>
+    </nav>
 
 
-                                </header><!-- .entry-header -->
+    <div class="container" id="start">
 
-                                <div class="entry-content">
-
-
-
-                                    <form action="http://not4you.se/studentadded.php" method="post">
-                                        <b>Add a New Student</b>
-                                        <p>First Name:
-                                            <input type="text" name="first_name" size="30" value="" />
-                                        </p>
-                                        <p>Last Name:
-                                            <input type="text" name="last_name" size="30" value="" />
-                                        </p>
-                                        <p>Email:
-                                            <input type="text" name="email" size="30" value="" />
-                                        </p>
-                                        <p>Street:
-                                            <input type="text" name="street" size="30" value="" />
-                                        </p>
-                                        <p>City:
-                                            <input type="text" name="city" size="30" value="" />
-                                        </p>
-                                        <p>State (2 Characters):
-                                            <input type="text" name="state" size="30" maxlength="2" value="" />
-                                        </p>
-                                        <p>Zip Code:
-                                            <input type="text" name="zip" size="30" maxlength="5" value="" />
-                                        </p>
-                                        <p>Phone Number:
-                                            <input type="text" name="phone" size="30" value="" />
-                                        </p>
-                                        <p>Birth Date (YYYY-MM-DD):
-                                            <input type="text" name="birth_date" size="30" value="" />
-                                        </p>
-                                        <p>Sex (M or F):
-                                            <input type="text" name="sex" size="30" maxlength="1" value="" />
-                                        </p>
-                                        <p>Lunch Cost:
-                                            <input type="text" name="lunch" size="30" value="" />
-                                        </p>
-                                        <p>
-                                            <input type="submit" name="submit" value="Send" class="button button-readmore"/>
-                                        </p>
-
-                                       
-
-                                    </form>
+        <form action="http://not4you.se/studentadded.php" method="post">
+            <b>Add a New Student</b>
+            <p>First Name:
+                <input type="text" name="first_name" size="30" value="" />
+            </p>
+            <p>Last Name:
+                <input type="text" name="last_name" size="30" value="" />
+            </p>
+            <p>Email:
+                <input type="text" name="email" size="30" value="" />
+            </p>
+            <p>Street:
+                <input type="text" name="street" size="30" value="" />
+            </p>
+            <p>City:
+                <input type="text" name="city" size="30" value="" />
+            </p>
+            <p>State (2 Characters):
+                <input type="text" name="state" size="30" maxlength="2" value="" />
+            </p>
+            <p>Zip Code:
+                <input type="text" name="zip" size="30" maxlength="5" value="" />
+            </p>
+            <p>Phone Number:
+                <input type="text" name="phone" size="30" value="" />
+            </p>
+            <p>Birth Date (YYYY-MM-DD):
+                <input type="text" name="birth_date" size="30" value="" />
+            </p>
+            <p>Sex (M or F):
+                <input type="text" name="sex" size="30" maxlength="1" value="" />
+            </p>
+            <p>Lunch Cost:
+                <input type="text" name="lunch" size="30" value="" />
+            </p>
+            <p>
+                <input type="submit" name="submit" value="Send" class="button button-readmore" />
+            </p>
 
 
-                                   
-                                </div><!-- .entry-content -->
 
-                            </article><!-- #post-## -->
-
-                        </main><!-- #main -->
-                    </div><!-- #primary -->
+        </form>
 
 
-                </div>
 
+    </div><!-- #content -->
 
-            </div><!-- #content -->
-
+    <div class="jumbotron text-center" style="margin-bottom:0">
+        <div class="site-info">
+            &copy;2020 NOT4YOU
+        </div>
+    </div>
 
 
 
