b37bb73975610eab31bb566f274b9766f9b55ad3
not4you <jonekm@kth.se>
2020-05-06 11:54:09 +0200

    login update

--- login.php
+++ login.php
@@ -6,79 +6,30 @@
 
     <title>NOT4YOU &#8211; F#!& OFF</title>
     <link rel="stylesheet" id='boostramp' href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <link rel='stylesheet' id='fullscreenly-parent-style-css' href='https://not4you.se/text/style.css' type='text/css' media='all' />
     <link rel='stylesheet' id='dashicons-css' href='https://not4you.se/text/dashicons.min.css?ver=5.4.1' type='text/css' media='all' />
 
-    <style type="text/css">
-        .image-header {
 
-            width: 100%;
-            height: 100%;
-            position: fixed;
-            top: 0;
-            left: 0;
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+ 
 
-            -webkit-backface-visibility: hidden;
-            backface-visibility: hidden;
-            -webkit-transform: translateZ(0) scale(1.0, 1.0);
-            transform: translateZ(0);
-            -ms-transform: translateZ(0);
-
-            background: ;
-
-            background-size: cover;
-            background-attachment: scroll;
-            -webkit-animation: grow 60s linear 10ms infinite;
-            animation: grow 60s linear 10ms infinite;
-
-            -webkit-transition: all 0.2s ease-in-out;
-            transition: all 0.2s ease-in-out;
-            z-index: -2;
-            background: #222 url(https://not4you.se/img/header.jpeg ) center center no-repeat;
-        }
-    </style>
+ 
 
 
 </head>
 
 <body>
 
-    <div class="header">
-        <div class="image-header"></div>
-        <div class="header-menu">
-            <div class="header-top">
+ <!-- logo -->
 
-                <!-- logo -->
+ <a href="https://not4you.se/" rel="home" class="logo">NOT4YOU</a>
 
-                <a href="https://not4you.se/" rel="home" class="logo">NOT4YOU</a>
-
-
-
-                <nav id="nav" class="top-navigation" role="navigation" aria-label="Top Menu">
-                    <span id="creativeily-menu-back"><span class="dashicons dashicons-arrow-left-alt2"></span></span>
-                    <span id="creativeily-menu-home"><span class="dashicons dashicons-admin-home"></span></span>
-
-                    <div id="top-menu" class="menu">
-                        <ul id="menu-menu-1" class="">
-                            <li id="menu-item-18" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-18">
-                                <a href="http://not4you.se/" aria-current="page">Home</a></li>
-                            <li id="menu-item-19" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-19">
-                                <a href="https://not4you.se/getstudentinfo.php">Show
-                                    table</a></li>
-                            <li id="menu-item-24" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-24">
-                                <a href="https://not4you.se/login.php">LOGIN</a>
-                            </li>
-                        </ul>
-                    </div> <a href="#" id="accessibility-close-menu"></a>
-                </nav><!-- #site-navigation -->
-            </div>
-        </div>
-
-
-    </div>
+ <div class="jumbotron text-center" style="margin-bottom:0">
+ 
+ <a href="https://not4you.se/" rel="home" class="logo">NOT4YOU</a> 
+</div>
 
 
     <div class="container" id="start">
