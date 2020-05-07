be53941e31d98b20706dac4b93ce3e7ff22d4300
not4you <jonekm@kth.se>
2020-05-07 09:37:18 +0200

    login fix

--- login.php
+++ login.php
@@ -26,7 +26,7 @@
 
 
     <div class="limiter">
-       
+        <div class="container-login100">
             <div class="wrap-login100">
                 <div class="login100-pic js-tilt" data-tilt>
                     <img src="images/img-01.png" alt="IMG">
@@ -81,11 +81,11 @@
 
 
 
-   
 
 
 
-   
+
+
 
     <div class="jumbotron text-center" style="margin-bottom:0">
         <div class="site-info">
