0c8352648cd337c423acf26a9bf810dab534a48d
not4you <jonekm@kth.se>
2020-05-10 19:01:54 +0200

    chart21

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-define('DB_SERVER', 'localhost');
-define('DB_USER', 'u469899741_esp_board');
-define('DB_PASSWORD', 'Grjw52E6%CYY');
-define('DB_NAME', 'u469899741_esp_data');
-define('TB_ENV', 'Sensor');
