068bfd9ca6e8642635a9c9b832ad7ea64c26d0a2
not4you <jonekm@kth.se>
2020-05-10 16:27:10 +0200

    chart5

--- config.php
+++ config.php
@@ -1,5 +1,12 @@
 <?php
-DEFINE ('DB_USER', 'u469899741_esp_board');
+DEFINE ('DB_USER', '');
 DEFINE ('DB_PASSWORD', 'Grjw52E6%CYY');
 DEFINE ('DB_HOST', 'localhost');
 DEFINE ('DB_NAME', 'u469899741_esp_data');
+define('TB_ENV', '--Name of table storing the data --');
+
+define('DB_SERVER', 'localhost');
+	define('DB_USER', 'u469899741_esp_board');
+	define('DB_PASSWORD', 'Grjw52E6%CYY');
+	define('DB_NAME', 'u469899741_esp_data');
+	define('TB_ENV', 'SensorData');
