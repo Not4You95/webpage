ffb2cb6022631163830f641f34a756bafec8137f
not4you <jonekm@kth.se>
2020-05-11 23:55:10 +0200

    login19

--- authenticate.php
+++ authenticate.php
@@ -15,7 +15,22 @@ if(isset($_POST['submit'])){
         exit('Please fill and password field!');
     }
 
+    $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME) or die('Could not connect to MySQL: ' .
+    mysqli_connect_error());
+
+    if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+        // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+        $stmt->bind_param('s', $_POST['username']);
+        $stmt->execute();
+        // Store the result so we can check if the account exists in the database.
+        $stmt->store_result();
     
+    
+        $stmt->close();
+    }
+
+
+    echo 'The end!';
 
 
 }
