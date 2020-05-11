aec25110152696c04ed47529d502070fbd835e87
not4you <jonekm@kth.se>
2020-05-11 23:30:53 +0200

    login19

--- authenticate.php
+++ authenticate.php
@@ -42,7 +42,27 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
     echo '..................';
     echo '';
 	// Store the result so we can check if the account exists in the database.
-	$stmt->store_result();
+    $stmt->store_result();
+    
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $password);
+        $stmt->fetch();
+        // Account exists, now we verify the password.
+        // Note: remember to use password_hash in your registration file to store the hashed passwords.
+        if (password_verify($_POST['password'], $password)) {
+            // Verification success! User has loggedin!
+            // Create sessions so we know the user is logged in, they basically act like cookies but remember the data on the server.
+            session_regenerate_id();
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $_POST['username'];
+            $_SESSION['id'] = $id;
+            echo 'Welcome ' . $_SESSION['name'] . '!';
+        } else {
+            echo 'Incorrect password!';
+        }
+    } else {
+        echo 'Incorrect username!';
+    }
 
 
     echo 'Allmost end!';
@@ -52,7 +72,3 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
 
 echo '';
 echo 'The End!';
-
-
-
-?>
