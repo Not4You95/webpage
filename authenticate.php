1abf8822480ba0edf51a6487d5687cf3bbe16958
not4you <jonekm@kth.se>
2020-05-11 22:59:15 +0200

    login15

--- authenticate.php
+++ authenticate.php
@@ -5,6 +5,17 @@ session_start();
 
 echo 'Hello world!';
 
+// Now we check if the data from the login form was submitted, isset() will check if the data exists.
+if (!isset($_POST['username'], $_POST['password'])) {
+    // Could not get the data that should have been sent.
+    exit('Please fill both the username and password fields!');
+}
+
+
+// Try and connect using the info above.
+$conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME) or die('Could not connect to MySQL: ' .
+    mysqli_connect_error());
+
 
 
 ?>
