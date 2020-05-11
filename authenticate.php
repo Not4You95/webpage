486c24eba19bdaa8b0b948c0ddfa372892b15328
not4you <jonekm@kth.se>
2020-05-11 23:14:02 +0200

    login18

--- authenticate.php
+++ authenticate.php
@@ -12,30 +12,42 @@ if (!isset($_POST['username'], $_POST['password'])) {
 }
 
 echo 'Post \n';
+echo;
 echo $_POST['username'];
+echo;
 echo $_POST['password'];
 
 // Try and connect using the info above.
 $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME) or die('Could not connect to MySQL: ' .
     mysqli_connect_error());
 
+echo;
 echo 'Database \n';
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+echo;
 echo $_POST['username'];
+echo;
 echo $_POST['password'];
 if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+    echo '.';
+    echo ;
     
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
 	$stmt->bind_param('s', $_POST['username']);
-	$stmt->execute();
+    $stmt->execute();
+    echo '..';
+    echo;
 	// Store the result so we can check if the account exists in the database.
 	$stmt->store_result();
 
 
+    echo 'Allmost end!'
+    echo;
 	$stmt->close();
 }
 
+echo;
 echo 'The End!';
 
 
