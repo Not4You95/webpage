53284f97f38302ce19e45f5ffbbfdeaa1c4bb8f8
not4you <jonekm@kth.se>
2020-05-11 23:19:10 +0200

    login18

--- authenticate.php
+++ authenticate.php
@@ -29,14 +29,15 @@ echo'';
 echo $_POST['username'];
 echo'';
 echo $_POST['password'];
+
 if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-    echo '.';
+    echo '-----------------------';
     echo '';
     
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
 	$stmt->bind_param('s', $_POST['username']);
     $stmt->execute();
-    echo '..';
+    echo '..................';
     echo '';
 	// Store the result so we can check if the account exists in the database.
 	$stmt->store_result();
