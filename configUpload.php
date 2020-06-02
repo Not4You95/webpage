04f2c5632f86eb809de0fb1c814d6915d1220bbd
Not4You <ekman7950@gmail.com>
2020-06-02 22:32:43 +0200

    update5

--- configUpload.php
+++ configUpload.php
@@ -233,15 +233,23 @@ if (!isset($_SESSION['loggedin'])) {
                                             <h4 class="card-title">Default form</h4>
                                             <p class="card-description"> Basic form layout </p>
                                             <form class="forms-sample">
-
-                                                
+                                                <div class="form-group">
+                                                    <label for="exampleInputEmail1">Email address</label>
+                                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Password</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                                </div>
                                                 <button type="submit" class="btn btn-success mr-2">Submit</button>
                                                 <button class="btn btn-light">Cancel</button>
                                             </form>
-                                            <form action="https://not4you.se/upload.php" method="post" enctype="multipart/form-data">
-                                                Select image to upload:
+
+                                            <form class="forms-sample" action="https://not4you.se/upload.php" method="post" enctype="multipart/form-data">
+                                                Select file:
+                                                
                                                 <input type="file" name="file" id="file">
-                                                <input type="submit"  class="btn btn-success mr-2" value="Upload Image" name="submit">
+                                                <input type="submit" value="Upload Image" name="submit">
                                             </form>
                                         </div>
                                     </div>
