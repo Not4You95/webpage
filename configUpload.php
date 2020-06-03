16cbac5a74894b3c76f5496e3386ecad55fc0ad9
Not4You <ekman7950@gmail.com>
2020-06-03 22:53:42 +0200

    update30

--- configUpload.php
+++ configUpload.php
@@ -15,7 +15,7 @@ include('includes/head.php');
                                 <p class="card-description"></p>
 
                                 <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">Device</label>
+                                    <label class="col-sm-3 col-form-label">Device type</label>
                                     <div class="col-sm-9">
                                         <select class="form-control">
                                             <option>ESP32</option>
@@ -26,10 +26,26 @@ include('includes/head.php');
                                     </div>
                                 </div>
 
+                                
+
+                                <form class="forms-sample" action="https://not4you.se/" method="post" enctype="multipart/form-data">
+                                    <div class="form-group row">
+                                    <label class="col-sm-3 col-form-label">Device</label>
+                                    <div class="col-sm-9">
+                                        <select class="form-control">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                </form>
+
                                 <form class="forms-sample" action="https://not4you.se/upload.php" method="post" enctype="multipart/form-data">
                                     Select file:
 
-                                    <input type="file" name="file" id="file"> <br>
+                                    <input type="file" name="file" id="file"> 
                                     <input class="btn btn-success mr-2" type="submit" value="Upload Image" name="submit">
                                 </form>
                             </div>
