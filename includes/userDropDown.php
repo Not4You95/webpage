dbd80022bad7692b744e4b318d53fc29b5b348c9
Not4You <ekman7950@gmail.com>
2020-06-03 22:35:33 +0200

    update21

---
+++ includes/userDropDown.php
@@ -0,0 +1,10 @@
+<div class="dropdown-header text-center">
+    <img class="img-md rounded-circle" src="../../../assets/images/faces-clipart/pic-4.jpg" alt="Profile image">
+    <p class="mb-1 mt-3 font-weight-semibold">Pink pantern</p>
+    <p class="font-weight-light text-muted mb-0">DancingMonkey@gmail.com</p>
+</div>
+<a class="dropdown-item">My Profile <span class="badge badge-pill badge-danger">1</span><i class="dropdown-item-icon ti-dashboard"></i></a>
+<a class="dropdown-item">Messages<i class="dropdown-item-icon ti-comment-alt"></i></a>
+<a class="dropdown-item">Activity<i class="dropdown-item-icon ti-location-arrow"></i></a>
+<a class="dropdown-item">FAQ<i class="dropdown-item-icon ti-help-alt"></i></a>
+<a href="https://not4you.se/logout.php" class="dropdown-item">Sign Out<i class="dropdown-item-icon ti-power-off"></i></a>
