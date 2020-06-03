c69b8e2f181b8fa544c3b491a571db112dbb2520
Not4You <ekman7950@gmail.com>
2020-06-03 22:42:24 +0200

    update26

--- includes/head.php
+++ includes/head.php
@@ -18,7 +18,7 @@ if (!isset($_SESSION['loggedin'])) {
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Not4You admin page</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="../../../assets/vendors/iconfonts/mdi/css/materialdesignicons.min.css">
