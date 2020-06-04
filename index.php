7d8fe4242338a36c094b54338e736da02acb9631
Not4You <ekman7950@gmail.com>
2020-06-04 22:50:31 +0200

    update56

--- index.php
+++ index.php
@@ -19,6 +19,43 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
 
 
 <!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <link href="text/loginStyle.css" rel="stylesheet" type="text/css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+
+<body>
+
+    <div class="container h-80">
+        <div class="row align-items-center h-100">
+            <div class="col-3 mx-auto">
+                <div class="text-center">
+                    <img id="profile-img" class="rounded-circle profile-img-card" src="https://i.imgur.com/6b6psnA.png" />
+                    <p id="profile-name" class="profile-name-card"></p>
+                    <form class="form-signin"  action="https://not4you.se/authenticate.php" method="post">
+
+                        <input type="username" name="username" id="username" class="form-control form-group" placeholder="username" required autofocus>
+                        <input type="password" name="password" id="inputPassword" class="form-control form-group" placeholder="password" required autofocus>
+                        <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">enter</button>
+                    </form><!-- /form -->
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+
+<!-- <!DOCTYPE html>
 <html>
 
 <head>
@@ -55,4 +92,4 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
 
 </body>
 
-</html>
+</html>-->
